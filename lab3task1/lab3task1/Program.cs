aa1d3d6d24ba40ea409765ef4e0db7b863ebcfb4
fizza bukhari <161758708+fizza555@users.noreply.github.com>
2024-04-04 10:51:35 -0700

    Add files via upload

---
+++ lab3task1/lab3task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string pattern = @"^(?:\d{1,6}(?:\.\d{0,6})?|\.\d{1,6})$";
+
+
+        while (true)
+        {
+            Console.WriteLine("Enter a floating-point number (with a length not greater than 6):");
+            string input = Console.ReadLine();
+
+        
+            if (Regex.IsMatch(input, pattern))
+            {
+                Console.WriteLine($"{input} is a valid floating-point number with length not greater than 6.");
+            }
+            else
+            {
+                Console.WriteLine($"{input} is not a valid floating-point number with length not greater than 6.");
+            }
+
+            Console.WriteLine("Do you want to check another number? (yes/no)");
+            string choice = Console.ReadLine().ToLower();
+            if (choice != "yes")
+                break;
+        }
+    }
+}
