5bdea7e4c539c755fb0a808f12cee516429eb793
fizza bukhari <161758708+fizza555@users.noreply.github.com>
2024-04-04 20:40:30 -0700

    Add files via upload

---
+++ lab2task1/lab2task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your input:");
+        string input = Console.ReadLine();
+        string pattern = @"(?:\|\||&&)";
+
+        MatchCollection matches = Regex.Matches(input, pattern);
+
+        if (matches.Count > 0)
+        {
+            Console.WriteLine("Logical operators found:");
+            foreach (Match match in matches)
+            {
+                Console.WriteLine("Logical operator: " + match.Value);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No logical operators found.");
+        }
+    }
+}
