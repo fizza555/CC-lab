a9a0ce48a27ac050292affef9c326878d8686cd0
fizza bukhari <161758708+fizza555@users.noreply.github.com>
2024-04-06 09:41:49 -0700

    Add files via upload

---
+++ Lab midterm/labmid3/labmid3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+class PasswordGenerator
+{
+    static Random random = new Random();
+
+    static string GeneratePassword(string firstName, string lastName, string regNumbers)
+    {
+        var pass = new StringBuilder();
+        pass.Append(char.ToLower(firstName[0])).Append(char.ToLower(lastName[0]));
+
+        foreach (var c in firstName.Where((_, i) => i % 2 != 0))
+            pass.Append(c);
+
+        foreach (var c in lastName.Where((_, i) => i % 2 == 0))
+            pass.Append(c);
+
+        pass.Append(char.ToUpper(lastName[random.Next(lastName.Length)]));
+
+        var numToAdd = 4 - regNumbers.Length;
+        if (numToAdd > 0)
+        {
+            pass.Append(regNumbers);
+            numToAdd -= regNumbers.Length;
+        }
+        for (int i = 0; i < numToAdd; i++)
+            pass.Append(random.Next(10));
+
+        var specialChars = "!@#$%^&*()-_=+[]{}|;:,<.>/?";
+        pass.Append(specialChars[random.Next(specialChars.Length)]);
+        pass.Append(specialChars[random.Next(specialChars.Length)]);
+
+        return pass.ToString().Substring(0, Math.Min(pass.Length, 16));
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write( " Please Enter your  first name: ");
+        var fName = Console.ReadLine();
+
+        Console.Write("Please Enter  your last name: ");
+        var lName = Console.ReadLine();
+
+        Console.Write("Please Enter  your registration number: ");
+        var regNums = Console.ReadLine();
+
+        var password = GeneratePassword(fName, lName, regNums);
+        Console.WriteLine(" unique Password  generated: " + password);
+    }
+}
